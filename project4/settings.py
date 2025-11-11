d247c059789fa61320fe55d4a56eab01731f972f
Vickoboy <vickoboy104@gmail.com>
2025-11-11 19:30:00 +0100

    Update ALLOWED_HOSTS for deployment

--- project4/settings.py
+++ project4/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.27.144.92', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'network-gl0t.onrender.com',
+    '*'
+]
+
 
 
 # Application definition
