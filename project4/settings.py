aa10ea3d1c48cbaee8c210ee425b130dc32ff4a3
Vickoboy <vickoboy104@gmail.com>
2025-11-12 09:34:50 +0100

    Add CSRF trusted origins for deployment

--- project4/settings.py
+++ project4/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#Add CSRF trusted origins for deployment
+CSRF_TRUSTED_ORIGINS = [
+    'https://thenetwork.up.railway.app',
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
